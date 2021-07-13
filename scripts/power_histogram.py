1ad56a087688e5d4330b47c32be57f61a6989856
msoham123 <msoham123@gmail.com>
2021-07-13 14:27:40 -0700

    Added more comments to document code

--- scripts/power_histogram.py
+++ scripts/power_histogram.py
@@ -1,6 +1,5 @@
 from matplotlib import pyplot as plt
 
-
 WEAK_THRESHOLD = 200
 MOD_THRESHOLD = 400
 
@@ -16,10 +15,11 @@ def power(row):
 
 def create_power_histogram(dataframe):
     print("Generating power histogram...")
+    # notify that plot is being generated
     dataframe["Power"] = dataframe.apply(power, axis=1)
     figure = plt.figure()
     plt.hist([dataframe["Power"]], bins=7, )
     plt.xlabel("Power")
     plt.title("Power Histogram")
     figure.savefig("plots/power_histogram.png", dpi=figure.dpi)
-    print("Finished. Output saved to plots/power_histogram.png.")
+    print("Finished. Output saved to plots/power_histogram.png.")  # notify that plot is created
--- scripts/regression_plot.py
+++ scripts/regression_plot.py
@@ -3,20 +3,22 @@ import matplotlib.pyplot as plt
 
 
 def create_regression_plot(dataframe, stat_one, stat_two):
-    print("Generating linear regression plot between " + stat_one + " and " + stat_two + "...")
     # notify that plot is being generated
+    print("Generating linear regression plot between " + stat_one + " and " + stat_two + "...")
     lin_regress = LinearRegression()
-    x = dataframe[stat_one].values.reshape(-1, 1)  # set x val
     # note the reshape is necessary if only one feature
-    y = dataframe[stat_two] # set y-val
+    x = dataframe[stat_one].values.reshape(-1, 1)  # set x val
+    y = dataframe[stat_two]  # set y-val
     lin_regress.fit(x, y)  # Create a regression model
     lin_predict = lin_regress.predict(x)  # Predict values based on regression model
-    figure = plt.figure() # create figure object
+    figure = plt.figure()  # create figure object
     plt.scatter(x, y, color='black')  # plot the actual data as black circles
-    plt.xlabel(stat_one) # set x label
-    plt.ylabel(stat_two) # set y label
-    plt.title(stat_one + " versus " + stat_two) # set title
-    plt.plot(x, lin_predict, 'cs')  # plot the expected results from the regression as cyan
-    figure.savefig("plots/"+stat_one+"versus"+stat_two+"_lreg.png", dpi=figure.dpi) # save the figure into loc
-    print("Finished. Output saved to " + "plots/"+stat_one+"versus"+stat_two+"_lreg.png") # notify that plot is created
-
+    plt.xlabel(stat_one)  # set x label
+    plt.ylabel(stat_two)  # set y label
+    plt.title(stat_one + " versus " + stat_two)  # set title
+    # plot the expected results from the regression as cyan
+    plt.plot(x, lin_predict, 'cs')
+    # save the figure into loc
+    figure.savefig("plots/" + stat_one + "versus" + stat_two + "_lreg.png", dpi=figure.dpi)
+    # notify that plot is created
+    print("Finished. Output saved to " + "plots/" + stat_one + "versus" + stat_two + "_lreg.png")
